d150aa29884f878d8c5a7478601c38119cbef5d5
dylancl <dylan_cathelyn@hotmail.be>
2021-05-31 13:48:48 +0200

    Fix import

--- server/src/controllers/publicController.js
+++ server/src/controllers/publicController.js
@@ -1,3 +1,5 @@
+import fetch from 'node-fetch';
+
 const URL = 'https://api.coingecko.com/api/v3/coins/markets?vs_currency=eur&order=market_cap_desc&per_page=100&page=1&sparkline=false';
 
 const getHome = async(req, res, next) => {

40b5a1f66c8b2608d06518141797d7ea76425d6e
pgm-arthtemm <arthur.temmerman@student.arteveldehs.be>
2021-05-31 13:29:40 +0200

    Added partials

--- server/src/services/dataService.js
+++ server/src/services/dataService.js
@@ -1,13 +1,13 @@
 const fetchData = async (url) => {
-    try {
-        const response = await fetch(url);
-        const data = await response.json();
-        console.log(data);
-    } catch (error) {
-        
-    }
+	try {
+		const response = await fetch(url);
+		const data = await response.json();
+		return data;
+	} catch (error) {
+	}
 };
 
+
 module.exports = {
-    fetchData,
+	fetchData,
 }

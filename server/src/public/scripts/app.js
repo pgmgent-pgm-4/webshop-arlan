510098b96a421ff956fdedba617a0ed1cd88cdd1
dylancl <dylan_cathelyn@hotmail.be>
2021-06-03 16:47:04 +0200

    Add GSAP for ticker

--- server/src/public/scripts/app.js
+++ server/src/public/scripts/app.js
@@ -1,5 +1,33 @@
-new Marquee('marqueeTicker', {
-  direction: 'rtl'
-});
+gsap.set(".ticker", { y: 0 });
 
-console.log('test');
+let boxWidth = 250,
+	totalWidth = boxWidth * 30, //  * n of boxes
+	no01 = document.querySelectorAll(".ticker__data--item"),
+	dirFromLeft = "+=" + totalWidth,
+	dirFromRight = "-=" + totalWidth;
+
+const mod = gsap.utils.wrap(0, totalWidth);
+
+const marquee = (which, time, direction) => {
+	gsap.set(which, {
+		x: function (i) {
+			return i * boxWidth;
+		}
+	});
+	let action = gsap.timeline().to(which, {
+		x: direction,
+		modifiers: {
+			x: (x) => mod(parseFloat(x)) + "px"
+		},
+		duration: time,
+		ease: "none",
+		repeat: -1
+	});
+	return action;
+};
+
+let master = gsap
+	.timeline()
+	.add(marquee(no01, 30, dirFromLeft), 1)
+
+// =============================

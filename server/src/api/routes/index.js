682312adc76162a6eac50d46e8cf717761e1e2c9
dylancl <dylan_cathelyn@hotmail.be>
2021-06-16 02:59:22 +0200

    Added all documentation for endpoints with Swagger, added product controllers

--- server/src/api/routes/index.js
+++ server/src/api/routes/index.js
@@ -99,6 +99,7 @@ router.post('/favorites', favoriteController.createFavorite);
  *                         example: 1
  */
 router.get('/orders', passport, orderController.getOrders);
+
 /**
  * @swagger
  * /api/orders/{orderId}:
@@ -126,40 +127,7 @@ router.get('/orders', passport, orderController.getOrders);
  *                 data:
  *                   type: array
  *                   items:
- *                     type: object
- *                     properties:
- *                       id:
- *                         type: integer
- *                         description: The order id
- *                         example: 1
- *                       date:
- *                         type: string
- *                         description: ISO date
- *                         example: 2021-06-14 10:10:09.983+00:00
- *                       price:
- *                         type: integer
- *                         description: Order price
- *                         example: 459
- *                       order_value:
- *                         type: integer
- *                         description: Amount of products bought for the order
- *                         example: 20
- *                       status: 
- *                         type: string
- *                         description: Order status
- *                         example: pending
- *                       createdAt:
- *                         type: text
- *                         description: Order creation date
- *                         example: 2021-06-14 10:10:09.983 +00:00
- *                       updatedAt:
- *                         type: text
- *                         description: Order update date
- *                         example: 2021-06-14 10:10:09.983 +00:00
- *                       UserId:
- *                         type: integer
- *                         description: ID of the user who placed the order
- *                         example: 1
+ *                     $ref: '#/definitions/Order'
  */
 router.get('/orders/:orderId', orderController.getOrderById);
 
@@ -336,11 +304,211 @@ router.get('/products', productController.getProducts);
  */
 router.get('/products/:id', productController.getProductById);
 
+/**
+ * @swagger
+ * /api/products/:
+ *   post:
+ *     tags:
+ *       - products
+ *     summary: Create a new product 
+ *     description: Create a new product using an object
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: '#/definitions/Product'
+ *     responses:
+ *       201:
+ *        description: Succesful POST operation
+ *        schema:
+ *          $ref: '#/definitions/Product'
+ *       400:
+ *          description: Invalid order body
+ */
+router.post('/products', productController.createProduct);
+
+/**
+ * @swagger
+ * /api/products/{id}:
+ *   put:
+ *     tags:
+ *       - products
+ *     summary: Update an existing product
+ *     description: Update an existing product using an object
+ *     parameters:
+ *     - in: path
+ *       name: id
+ *       required: true
+ *       schema:
+ *        type: string
+ *       description: Product ID
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: '#/definitions/Product'
+ *     responses:
+ *       201:
+ *        description: Succesful POST operation
+ *        schema:
+ *          $ref: '#/definitions/Product'
+ *       400:
+ *          description: Invalid order body
+ */
+router.put('/products/:id', productController.updateProduct);
+
+/**
+ * @swagger
+ * /api/products/{id}:
+ *   delete:
+ *     tags:
+ *       - products
+ *     summary: Delete an existing product
+ *     description: Delete an existing product using productId
+ *     parameters:
+ *     - in: path
+ *       name: id
+ *       required: true
+ *       schema:
+ *        type: string
+ *        description: Product ID
+ *     responses:
+ *       201:
+ *        description: Succesful POST operation
+ *       400:
+ *          description: Invalid id
+ */
+router.delete('/products/:id', productController.deleteProduct);
+
+
+/**
+ * @swagger
+ * /api/productReviews/:
+ *   get:
+ *     summary: Retrieve all product reviews
+ *     tags:
+ *       - product_reviews
+ *     description: Retrieve an array of objects, each object containing a product review
+ *     responses:
+ *       200:
+ *         description: A list of product reviews
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 data:
+ *                   type: array
+ *                   items:
+ *                     $ref: '#/definitions/Product_review'
+ */
 router.get('/productReviews', productReviewsController.getProductReviews);
+
+/**
+ * @swagger
+ * /api/productReviews/{productReviewId}:
+ *   get:
+ *     tags:
+ *       - product_reviews
+ *     summary: Get an existing product review
+ *     description: Get an existing product review using productReviewId
+ *     parameters:
+ *     - in: path
+ *       name: productReviewId
+ *       required: true
+ *       schema:
+ *        type: integer
+ *        minimum: 1
+ *       description: productReviewId
+ *     responses:
+ *       201:
+ *        description: Succesful POST operation
+ *       400:
+ *          description: Invalid orderId
+ */
 router.get('/productReviews/:productReviewId', productReviewsController.getProductReviewById);
+
+/**
+ * @swagger
+ * /api/productReviews/:
+ *   post:
+ *     tags:
+ *       - product_reviews
+ *     summary: Create a new product review
+ *     description: Create a new product review using an object
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: '#/definitions/Product_review'
+ *     responses:
+ *       201:
+ *        description: Succesful POST operation
+ *        schema:
+ *          $ref: '#/definitions/Product_review'
+ *       400:
+ *          description: Invalid order body
+ */
 router.post('/productReviews', productReviewsController.createProductReview);
-router.put('/productReviews', productReviewsController.updateProductReview);
-router.delete('/productReview', productReviewsController.deleteProductReview);
+
+/**
+ * @swagger
+ * /api/orders/{productReviewId}:
+ *   put:
+ *     tags:
+ *       - product_reviews
+ *     summary: Update an existing product review
+ *     description: Update an existing product review using an object
+ *     parameters:
+ *     - in: path
+ *       name: productReviewId
+ *       required: true
+ *       schema:
+ *        type: integer
+ *        minimum: 1
+ *       description: Product Review ID
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: '#/definitions/Product_review'
+ *     responses:
+ *       201:
+ *        description: Succesful POST operation
+ *        schema:
+ *          $ref: '#/definitions/Product_review'
+ *       400:
+ *          description: Invalid order body
+ */
+router.put('/productReviews/:productReviewId', productReviewsController.updateProductReview);
+
+/**
+ * @swagger
+ * /api/orders/{productReviewId}:
+ *   delete:
+ *     tags:
+ *       - product_reviews
+ *     summary: Delete an existing product review
+ *     description: Delete an existing product review using productReviewId
+ *     parameters:
+ *     - in: path
+ *       name: productReviewId
+ *       required: true
+ *       schema:
+ *        type: integer
+ *        minimum: 1
+ *       description: Product Review ID
+ *     responses:
+ *       201:
+ *        description: Succesful POST operation
+ *       400:
+ *          description: Invalid productReviewId
+ */
+router.delete('/productReview/:productReviewId', productReviewsController.deleteProductReview);
 
 router.post('/register', authenticationController.handleRegister);
 router.post('/login', authenticationController.handleLogin);
@@ -376,8 +544,6 @@ export default router;
  *         format: int32
  *         minimum: 1 
  *         maximum: 10
- *     xml:
- *       name: Order
  *   Product:
  *     type: object
  *     properties:
@@ -386,6 +552,31 @@ export default router;
  *         enum:
  *         - BITCOIN
  *         - ETHEREUM
+ *         - LITECOIN
+ *         - WOMENCOIN
+ *   Product_review:
+ *     type: object
+ *     properties:
+ *       message:
+ *         type: string
+ *         enum:
+ *         - wow!
+ *         - changed my life!
+ *         - I went from being broke and lonely to being rich and lonely!
+ *       rating:
+ *         type: integer
+ *         format: int32
+ *         minimum: 1
+ *         maximum: 10
+ *       UserId:
+ *         type: integer
+ *         format: int32
+ *         minimum: 1
+ *         maximum: 25
+ *       ProductId:
+ *         type: string
+ *         enum:
+ *         - BITCOIN
  *         - LITECOIN
  *         - WOMENCOIN
  */
